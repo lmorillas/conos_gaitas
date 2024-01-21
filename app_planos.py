a447f366782d4c8754e37c2808ad8b824273eba1
Luis Miguel <morillas@gmail.com>
2024-01-21 10:50:23 +0100

    typo

--- app_planos.py
+++ app_planos.py
@@ -101,7 +101,7 @@ def mapa(puntero: dict):
                    attr = 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
     )
 
-    '''
+    tiles = '''
 var Stadia_StamenTerrainBackground = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_terrain_background/{z}/{x}/{y}{r}.{ext}', {
 	minZoom: 0,
 	maxZoom: 18,
